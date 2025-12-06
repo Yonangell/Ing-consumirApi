27751f79e8b9d1a1a1701c5da4c5be34d0369b2f
Yonangell Guillen <121691054+Yonangell@users.noreply.github.com>
2025-12-06 19:12:52 +0100

    Agrenado correciones para mejorar la logica del proyecto

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnAnterior = document.getElementById("btn-anterior");
   const btnSiguiente = document.getElementById("btn-siguiente");
 
+  const searchInput = document.getElementById("pokemon-id");
+  const searchBtn = document.getElementById("search-btn");
+  const searchResultContainer = document.getElementById(
+    "search-result-container"
+  );
+
   let proximoUrl = null;
   let antesUrl = null;
 
@@ -21,9 +27,10 @@ document.addEventListener("DOMContentLoaded", () => {
     mostrandoEstado("Se esta cargando los pokemons...");
     btnAnterior.disabled = true;
     btnSiguiente.disabled = true;
+    searchResultContainer.innerHTML = "";
   }
 
-  function renderPokemon(pokemon) {
+  function renderPokemon(pokemon, parentContainer) {
     const tarjeta = document.createElement("div");
     tarjeta.className =
       "bg-white p-6 rounded-lg shadow-lg transform hover:scale-105 transition duration-300";
@@ -31,6 +38,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     const imageUrl =
       pokemon.sprites.front_default || "https://via.placeholder.com/250x150";
+
+    let imageHtml;
+
+    if(imageUrl.includes("placeholder.com")) {
+      imageHtml = `<div class="mx-auto h-32 w-32 mb-4 flex items-center justify-center text-center text-gray-600 border rounded-lg">Sin imagen oficial para mostrar.</div>`;
+    } else {
+      imageHtml = `<img src="${imageUrl}" alt="${name}" class="mx-auto h-32 w-32 mb-4">`;
+    }
 
     tarjeta.innerHTML = `
             <img src="${imageUrl}" alt="${name}" class="mx-auto h-32 w-32 mb-4">
@@ -50,11 +65,13 @@ document.addEventListener("DOMContentLoaded", () => {
                   .join("")}
             </div>
         `;
-    container.appendChild(tarjeta);
+    parentContainer.appendChild(tarjeta);
   }
 
   async function fetchPokemones(url = API_BASE_URL) {
     setCargando();
+
+    searchResultContainer.innerHTML = "";
 
     try {
       const response = await fetch(url);
@@ -78,13 +95,47 @@ document.addEventListener("DOMContentLoaded", () => {
       );
       const detallesPokemones = await Promise.all(pokemonPromesa);
 
-      detallesPokemones.forEach(renderPokemon);
+      detallesPokemones.forEach((p) => renderPokemon(p, container));
     } catch (error) {
       console.error("Hubo un problema con la operacion fetch:", error);
       mostrandoEstado(`Fallo la carga de datos ${error.message}`, true);
 
       btnAnterior.disabled = !antesUrl;
       btnSiguiente.disabled = !proximoUrl;
+    }
+  }
+
+  async function fetchPokemonNameOrById() {
+    const entrada = searchInput.value.trim().toLowerCase();
+
+    if (entrada === "") {
+      searchResultContainer.innerHTML = `<p class="text-red-500 p-4 bg-white rounded-lg">Por favor, ingrese un nombre o ID de un Pokémon.</p>`;
+      return;
+    }
+
+    searchResultContainer.innerHTML = `<p class="text-blue-500 p-4 bg-white rounded-lg">Buscando Pokémon: ${entrada}...</p>`;
+
+    container.innerHTML = "";
+    estado.className = "hidden";
+
+    try {
+      const response = await fetch(`${API_BASE_URL}${entrada}`);
+
+      if (!response.ok) {
+        throw new Error(
+          "Pokemon no encontrado. Intenta con otro nombre o ID."
+        );
+      }
+
+      const data = await response.json();
+
+      searchResultContainer.innerHTML = "";
+      renderPokemon(data, searchResultContainer);
+
+      searchInput.value = "";
+
+    } catch (error) {
+      searchResultContainer.innerHTML = `<p class="text-red-500 p-4 bg-white rounded-lg">${error.message}</p>`;
     }
   }
 
@@ -97,6 +148,14 @@ document.addEventListener("DOMContentLoaded", () => {
   btnAnterior.addEventListener("click", () => {
     if (antesUrl) {
       fetchPokemones(antesUrl);
+    }
+  });
+
+  searchBtn.addEventListener("click", fetchPokemonNameOrById);
+
+  searchInput.addEventListener("keydown", (evento) => {
+    if (evento.key === "Enter") {
+      fetchPokemonNameOrById();
     }
   });
 
